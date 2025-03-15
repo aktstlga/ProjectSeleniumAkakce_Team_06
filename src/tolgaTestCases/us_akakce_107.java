d56ced927d5bc9dc87d0e43e771bfde4a62ab1a8
AktsT <akts.tlga@gmail.com>
2025-03-15 23:11:06 +0300

    I used nth-child by the id of div, ı founded EmailPlaceholder and Password placeholder. (The Project forbid us to use ID and Name directly.)

--- src/tolgaTestCases/us_akakce_107.java
+++ src/tolgaTestCases/us_akakce_107.java
@@ -1,16 +1,25 @@
 package tolgaTestCases;
 
 import Utlity.BaseDriver;
+import Utlity.Myfunc;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class us_akakce_107 extends BaseDriver {
-   @Test
-    public void deletingAccountOnAkakce(){
-       driver.get("https://www.akakce.com");
+    @Test
+    public void deletingAccountOnAkakce() throws InterruptedException {
+        driver.get("https://www.akakce.com");
 
-       WebElement loginButton = driver.findElement(By.cssSelector("a[href='/akakcem/giris/']"));
-       loginButton.click();
-   }
+        WebElement loginButton = driver.findElement(By.cssSelector("a[href='/akakcem/giris/']"));
+        loginButton.click();
+
+        WebElement emailPlaceholder = driver.findElement(By.cssSelector("[id='FrmLi']> :nth-child(4) input"));
+        emailPlaceholder.sendKeys("team006test@gmail.com");
+        Thread.sleep(3000);
+
+        WebElement passwordPlaceholder = driver.findElement(By.cssSelector("[id='FrmLi']> :nth-child(6) input"));
+        passwordPlaceholder.sendKeys("Password123");
+
+    }
 }
