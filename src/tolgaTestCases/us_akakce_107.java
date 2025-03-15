607e657f0e40a24269fd22674208a84c08a237cb
AktsT <akts.tlga@gmail.com>
2025-03-15 21:49:16 +0300

    Created Chromdriver names as driver from WebDriver class for automation and adjusted the browser.

--- src/tolgaTestCases/us_akakce_107.java
+++ src/tolgaTestCases/us_akakce_107.java
@@ -1,4 +1,17 @@
 package tolgaTestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class us_akakce_107 {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        
+    }
 }
