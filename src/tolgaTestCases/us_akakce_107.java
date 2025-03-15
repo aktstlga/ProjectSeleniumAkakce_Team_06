da41ae354715c619eb22fa7073d891f5f435bc11
AktsT <akts.tlga@gmail.com>
2025-03-15 22:15:04 +0300

    I created a variable named as "Loginbutton" and used Selenium library to find it by it's link and click on it.

--- src/tolgaTestCases/us_akakce_107.java
+++ src/tolgaTestCases/us_akakce_107.java
@@ -2,6 +2,7 @@ package tolgaTestCases;
 
 import Utlity.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class us_akakce_107 extends BaseDriver {
@@ -9,6 +10,7 @@ public class us_akakce_107 extends BaseDriver {
     public void deletingAccountOnAkakce(){
        driver.get("https://www.akakce.com");
 
-
+       WebElement loginButton = driver.findElement(By.cssSelector("a[href='/akakcem/giris/']"));
+       loginButton.click();
    }
 }
