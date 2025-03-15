5047bf01bc6146aad712abcd09d54ee053b19c3f
AktsT <akts.tlga@gmail.com>
2025-03-15 22:13:30 +0300

    I used the created Utility commands to access "akakce" website.

--- src/tolgaTestCases/us_akakce_107.java
+++ src/tolgaTestCases/us_akakce_107.java
@@ -1,4 +1,14 @@
 package tolgaTestCases;
 
-public class us_akakce_107 {
+import Utlity.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+public class us_akakce_107 extends BaseDriver {
+   @Test
+    public void deletingAccountOnAkakce(){
+       driver.get("https://www.akakce.com");
+
+
+   }
 }
