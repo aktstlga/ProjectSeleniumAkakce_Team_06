c687bc4a6be2d5f4e589fcb19494fb9dcecdd830
AktsT <akts.tlga@gmail.com>
2025-03-16 15:23:21 +0300

    I added deleteAccount method and changed the name of first method to deleteAccountFailed.

--- src/tolgaTestCases/us_akakce_107.java
+++ src/tolgaTestCases/us_akakce_107.java
@@ -1,7 +1,6 @@
 package tolgaTestCases;
 
 import Utlity.BaseDriver;
-import Utlity.Myfunc;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,7 +8,7 @@ import org.openqa.selenium.WebElement;
 
 public class us_akakce_107 extends BaseDriver {
     @Test
-    public void deletingAccountOnAkakce() throws InterruptedException {
+    public void deleteAccountFailed() throws InterruptedException {
         driver.get("https://www.akakce.com");
 
         WebElement loginButton = driver.findElement(By.cssSelector("a[href='/akakcem/giris/']"));
@@ -38,7 +37,7 @@ public class us_akakce_107 extends BaseDriver {
         Thread.sleep(3000);
 
         WebElement passwordForDelete = driver.findElement(By.cssSelector("input.t[type='password']"));
-        passwordForDelete.sendKeys("ASD");
+        passwordForDelete.sendKeys("test123"); //InvalidPassword
         Thread.sleep(3000);
 
         WebElement deleteButton = driver.findElement(By.cssSelector("input[value='Hesabımı sil']"));
@@ -47,5 +46,43 @@ public class us_akakce_107 extends BaseDriver {
 
         WebElement warningOnFalsePassword = driver.findElement(By.cssSelector("div.alertX.t2 p"));
         Assert.assertTrue("Uyarı mesajı doğru verilemedi.", warningOnFalsePassword.getText().contains("Mevcut şifrenizi doğru girdiğinizden emin olun."));
+    }
+    @Test
+    public void deleteAccount() throws InterruptedException {
+        driver.get("https://www.akakce.com");
+
+        WebElement loginButton = driver.findElement(By.cssSelector("a[href='/akakcem/giris/']"));
+        loginButton.click();
+
+        WebElement emailPlaceholder = driver.findElement(By.cssSelector("[id='FrmLi']> :nth-child(4) input"));
+        emailPlaceholder.sendKeys("akts.tlga@gmail.com");
+        Thread.sleep(3000);
+
+        WebElement passwordPlaceholder = driver.findElement(By.cssSelector("[id='FrmLi']> :nth-child(6) input"));
+        passwordPlaceholder.sendKeys("30032003tlGA");
+        Thread.sleep(3000);
+
+        WebElement girisYapButton = driver.findElement(By.cssSelector("input.s[value='Giriş yap']"));
+        girisYapButton.click();
+        Thread.sleep(3000);
+
+        //driver.navigate().to("https://www.akakce.com/akakcem/");
+        WebElement myAccountAccess = driver.findElement(By.cssSelector("a[rel='nofollow'][href='/akakcem/']"));
+        myAccountAccess.click();
+        Thread.sleep(3000);
+
+        WebElement accessDeletePageButton = driver.findElement(By.cssSelector("a[href='/akakcem/kullanici-bilgilerim/uyelik-iptali/']"));
+        accessDeletePageButton.click();
+        Thread.sleep(3000);
+
+        WebElement passwordForDelete = driver.findElement(By.cssSelector("input.t[type='password']"));
+        passwordForDelete.sendKeys("30032003tlGA");
+        Thread.sleep(3000);
+
+        WebElement deleteButton = driver.findElement(By.cssSelector("input[value='Hesabımı sil']"));
+        deleteButton.click();
+        Thread.sleep(3000);
+
+        driver.quit();
     }
 }
